db76b8cf90a9e6dc94f57404e1e6183679e413a7
worldmozara <worldmozara@163.com>
2025-05-20 03:23:59 +0800

    update config docs

--- src/nonebot_plugin_ark_roulette/config.py
+++ src/nonebot_plugin_ark_roulette/config.py
@@ -3,8 +3,10 @@ from pydantic import BaseModel
 
 class Config(BaseModel, extra="ignore"):
     proxy: str | None = None
+    """HTTPX proxy（代理）参数"""
 
     update_on_launch: bool = True
+    """配置是否在启动时自动下载资源"""
 
 # # 调用 rebuild() 确保类完全定义
 # Config.model_rebuild()
